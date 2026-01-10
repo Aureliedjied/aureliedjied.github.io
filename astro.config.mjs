86f5cb290b902e556e475481fdb13c6b5b70a546
DJIED Aurélie <aurelie.djied@gmail.com>
2026-01-10 13:40:29 +0100

    ajout mode light

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import pagefind from "astro-pagefind";
 
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: 'https://aureliedjied.github.io',
-  integrations: [mdx()],
+  integrations: [mdx(), pagefind()],
 
   vite: {
     plugins: [tailwindcss()],
